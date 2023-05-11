9b005dcbe024caa8e2d33b14931899dcad8c5321
D00F209 <gabrieleraul.marini@nexigroup.com>
2023-05-11 12:49:28 +0200

    more apis..

--- src/main/java/com/nexi/letmeeat/rs/StdApi.java
+++ src/main/java/com/nexi/letmeeat/rs/StdApi.java
@@ -1,11 +1,13 @@
 package com.nexi.letmeeat.rs;
 
+import com.nexi.letmeeat.model.Booking;
 import com.nexi.letmeeat.model.Menu;
 import com.nexi.letmeeat.model.Order;
 import com.nexi.letmeeat.model.Restaurant;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -13,9 +15,14 @@ import java.util.List;
 
 public interface StdApi {
 
-    @GetMapping(value = "/order", produces = {"application/json"})
+    @PostMapping(value = "/order", consumes = {"application/json"})
     ResponseEntity<Void> postOrder(
             @RequestBody Order order)
+            throws Exception;
+
+    @PostMapping(value = "/order", produces = {"application/json"})
+    ResponseEntity<Void> postBooking(
+            @RequestBody Booking booking)
             throws Exception;
 
     @GetMapping(value = "/restaurant", produces = {"application/json"})
