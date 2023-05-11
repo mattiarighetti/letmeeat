8d78eb1b5e5ebc0514888638a67bf0da4c95c2d1
D00F209 <gabrieleraul.marini@nexigroup.com>
2023-05-11 12:06:14 +0200

    initial commit

---
+++ src/main/java/com/nexi/letmeeat/LetmeeatApplication.java
@@ -0,0 +1,13 @@
+package com.nexi.letmeeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmeeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetmeeatApplication.class, args);
+	}
+
+}
