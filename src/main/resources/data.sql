34fb6c244eff2110d9ea58fcf94296afdf2586bc
Mattia Righetti <mattia.righetti@nexi.it>
2023-05-11 18:24:14 +0200

    Dummy data update.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 --RESTAURANTS
-INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY) VALUES ('1', 'Ristorantino', 'ITALIAN');
-INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY) VALUES ('2', 'Baretto', 'BAR');
-INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY) VALUES ('3', 'Da Gino', 'PIZZA');
-
+INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('1', 'Hamburgherello', 'hamburger', 'https://media.timeout.com/images/105187765/image.jpg');
+INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('2', 'Er fusillo', 'pasta', 'https://assets.suitcasemag.com/images/landscape/198751-pasta-table-spread.jpg');
+INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('3', 'Da Gino', 'pizza', 'https://lh5.googleusercontent.com/p/AF1QipObQc9QrNuvVNnm35xYjz7GJforL0REpY8e0D6r=w408-h268-k-no');
+INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('4', 'Drinkesso', 'drinks', 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80');
+INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('5', 'Night shade', 'drinks', 'https://picjumbo.com/wp-content/uploads/glasses-with-champagne-nye-party-celebration-night-drinks-free-photo-2210x1473.jpg');
