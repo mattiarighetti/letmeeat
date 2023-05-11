24da1b7088b518c07c106ec754e350b9eda4557b
D00F209 <gabrieleraul.marini@nexigroup.com>
2023-05-11 14:18:46 +0200

    cose...

--- src/main/java/com/nexi/letmeeat/model/Dish.java
+++ src/main/java/com/nexi/letmeeat/model/Dish.java
@@ -1,4 +1,36 @@
 package com.nexi.letmeeat.model;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dishes")
+@DynamicUpdate
+@Data
 public class Dish {
+
+    @Id
+    @Column(name = "dish_id")
+    private Long dishId;
+
+    @Column
+    @JsonProperty
+    private String name;
+
+    @Column
+    @JsonProperty
+    private String image;
+
+
+    @Column
+    @JsonProperty
+    private String price;
+
 }
