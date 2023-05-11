2dfa67f97b19958b18a0126d85ffb000bbda008d
Mattia Righetti <mattia.righetti@nexi.it>
2023-05-11 18:40:58 +0200

    Dummy menu

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,20 @@ INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('2', 
 INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('3', 'Da Gino', 'pizza', 'https://lh5.googleusercontent.com/p/AF1QipObQc9QrNuvVNnm35xYjz7GJforL0REpY8e0D6r=w408-h268-k-no');
 INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('4', 'Drinkesso', 'drinks', 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80');
 INSERT INTO RESTAURANTS (RESTAURANT_ID, NAME, CATEGORY, IMAGE_URL) VALUES ('5', 'Night shade', 'drinks', 'https://picjumbo.com/wp-content/uploads/glasses-with-champagne-nye-party-celebration-night-drinks-free-photo-2210x1473.jpg');
+
+--MENUS
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('1', 'Hamburgerini tosti tosti', '1', '1');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('2', 'Pasta elicoidalissima', '1', '2');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('3', 'Pizzette', '1', '3');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('4', 'Pizzone', '1', '3');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('5', 'Gin tonic', '1', '4');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('6', 'Grappe & polente', '1', '4');
+INSERT INTO .MENUS (MENU_ID, NAME, RESTAURANT_ID) VALUES ('7', 'Long drinks', '5');
+INSERT INTO .MENUS (MENU_ID, NAME, AVAILABLE, RESTAURANT_ID) VALUES ('8', 'Short drinks', '1', '5');
+
+--DISHES
+
+--TABLES
+
+--SEATS
+
