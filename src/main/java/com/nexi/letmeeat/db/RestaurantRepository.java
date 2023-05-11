ed7e18b6c934e656283d936dbaf1e78afe8b1444
D00F209 <gabrieleraul.marini@nexigroup.com>
2023-05-11 21:10:13 +0200

    fix

--- src/main/java/com/nexi/letmeeat/db/RestaurantRepository.java
+++ src/main/java/com/nexi/letmeeat/db/RestaurantRepository.java
@@ -1,11 +1,10 @@
 package com.nexi.letmeeat.db;
 
 import com.nexi.letmeeat.model.Restaurant;
-import com.nexi.letmeeat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>,
+public interface RestaurantRepository extends JpaRepository<Restaurant, String>,
         JpaSpecificationExecutor<Restaurant> {
 
 }
--- src/main/java/com/nexi/letmeeat/db/TableRepository.java
+++ src/main/java/com/nexi/letmeeat/db/TableRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface TableRepository extends JpaRepository<Tables, Long>, JpaSpecificationExecutor<Tables> {
 
-    List<Tables> findTablesByRestaurantId(String restaurantId);
+    List<Tables> findTablesByRestaurant(String restaurantId);
 
 }
